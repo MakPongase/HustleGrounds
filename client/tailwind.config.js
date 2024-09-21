0017da947a618243e982b1512ab7be2c4f9bf0f6
Mak <macmacpongs02@gmail.com>
2024-09-21 13:46:34 +0800

    initialize project template with react and tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Include jsx, tsx as well
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
