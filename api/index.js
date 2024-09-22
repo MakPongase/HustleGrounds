dd82f3654f35f3a3d53a489d307a2627f74858ca
Mak <macmacpongs02@gmail.com>
2024-09-22 22:16:54 +0800

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -12,12 +13,14 @@ mongoose
     console.log(err);
   });
 const app = express();
-
+app.use(express.json());
 // app.get("/", (req, res) => {
 //   res.send("Hello World");
 // });
 
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+
 app.listen(3000, () => {
   console.log("Server aa is running on http://localhost:3000");
 });
